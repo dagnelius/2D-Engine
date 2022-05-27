563741645c47b578f9d20ab9bcb1e3429ec7656b
Dag Nelius <dag.nelius@gmail.com>
2022-05-27 17:54:26 +0300

    Minor Updates

--- TextureManager.h
+++ TextureManager.h
@@ -1,3 +1,6 @@
+#ifndef TextureManager_h
+#define TextureManager_h
+
 #include "Game.h"
 
 class TextureManager {
@@ -5,3 +8,6 @@ class TextureManager {
 public:
     static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* ren);
 };
+
+#endif /* TextureManager_h */
+
