cea715b5363c7ae24cf8451e79bea6b2f271ee29
Dag Nelius <dag.nelius@gmail.com>
2022-05-27 15:52:05 +0300

    Add TextureManager

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,8 @@
-#include "Game.hpp"
+#include "Game.h"
+#include "TextureManager.h"
+
+SDL_Texture* playerTex;
+SDL_Rect srcR, dstR;
 
 Game::Game()
 {}
@@ -22,11 +26,17 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
         
         renderer = SDL_CreateRenderer(window, -1, 0);
         if(renderer) {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
             std::cout << "Renderer Created!" << std::endl;
         }
         
         isRunning = true;
+        
+        playerTex = TextureManager::LoadTexture("../../../Assets/man.bmp", renderer);
+
+        std::cout << SDL_GetError() << std::endl;
+        
+        
     } else {
         
         isRunning = false;
@@ -49,11 +59,15 @@ void Game::handleEvents() {
 
 void Game::update() {
     cnt++;
+    dstR.h = 64;
+    dstR.w = 64;
+    dstR.x = cnt;
     std::cout << cnt << std::endl;
 }
 
 void Game::render() {
     SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, playerTex, NULL, &dstR);
     SDL_RenderPresent(renderer);
 }
 
---
+++ TextureManager.h
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+class TextureManager {
+
+public:
+    static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* ren);
+};
